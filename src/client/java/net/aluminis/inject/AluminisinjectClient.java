6d6e49dd24169a67e050c33424be87cb937a354f
Kaleb Gilson <kaleb.gilson@und.edu>
2025-03-19 21:08:07 -0500

    initial commit

---
+++ src/client/java/net/aluminis/inject/AluminisinjectClient.java
@@ -0,0 +1,10 @@
+package client.java.net.aluminis.inject;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AluminisinjectClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
